04de3cf72154f0c528c69cd129b07ecb3767648e
Igor Becker <igor_nyu@hotmail.com>
2023-05-26 15:50:01 -0300

    stack and 2crystal - day2

--- src/day2/Stack.ts
+++ src/day2/Stack.ts
@@ -1,18 +1,44 @@
+interface Node<T> {
+    prev?: Node<T>
+    value: T
+}
+
 export default class Stack<T> {
     public length: number;
-
-    
+    public head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        this.length++;
+        const newNode = { value: item } as Node<T>
 
-}
+        if (!this.head) {
+            this.head = newNode
+            return
+        }
+
+        const head = this.head
+        this.head = newNode
+        this.head.prev = head
+    }
+
     pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1)
 
-}
+        if (!this.head) {
+            return
+        }
+
+        const head = this.head
+        this.head = head.prev
+        return head.value
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value
+    }
 }
--- src/day2/TwoCrystalBalls.ts
+++ src/day2/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const step = Math.floor(Math.sqrt(3));
 
+    let i = 0;
+
+    for (; i + step < breaks.length; i += step) {
+        if (breaks[i]) {
+            break
+        }
+    }
+    
+    let j = i - step
+
+    for (; j < i+step; ++j) {
+        if (breaks[j]) {
+            return j
+        }
+    }
+
+    return -1
 }
